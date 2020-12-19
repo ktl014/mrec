7b5bb914b698ccaef0d34cc1c7e017117a05fcc3
Kevin Tran-Vu Le <Kevin.T.Le1@aexp.com>
2020-12-18 18:05:52 -0800

    feat(display-pred): Add starter code to app for displaying predictions

--- mrec/app.py
+++ mrec/app.py
@@ -4,6 +4,7 @@ Module is used to launch classification system
 
 """
 # Standard Dist Imports
+from datetime import datetime
 import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
@@ -16,6 +17,7 @@ import streamlit as st
 from mrec.data.dataset import load_data
 from mrec.visualization import SessionState
 import mrec.config as config
+from mrec.model.MREClassifier import MREClassifier
 
 #=== Load dataset as cache ===#
 @st.cache
@@ -34,8 +36,8 @@ def load_data_to_cache(csv_fnames):
     return val
 
 def run():
-    data, id = None, 0
-    session_state = SessionState.get(data=data, id=id)
+    data, predictions, id = None, [], 0
+    session_state = SessionState.get(id=id, data=data, predictions=predictions)
 
     dataset = load_data_to_cache(config.CSV_FNAMES)
 
@@ -51,6 +53,21 @@ def run():
     st.subheader('Raw Data')
     if st.checkbox('Show data') and (session_state.id != 0).all():
         st.dataframe(session_state.data)
+
+    # === Classification button ===#
+    # Recommend based off id
+    st.header('Classify Relationship')
+    recommend = st.button('Click here to Classify Relationship')
+    if recommend:
+        classifier = MREClassifier(model_weights='temp-model-weights')
+        session_state.predictions = classifier.predict(X=session_state.data, features_names='temp-feature-names')
+
+        # Report the predictions results
+        st.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | USR ID: {session_state.id}]')
+        st.write(f'Prediction: {session_state.predictions}')
+
+    else:
+        st.write("Press the above button to classify")
 
 if __name__ == '__main__':
     st.title('MREC: Medical Relation Extraction Classification')
--- mrec/model/MREClassifier.py
+++ mrec/model/MREClassifier.py
@@ -3,7 +3,10 @@ import joblib
 class MREClassifier(object):
 
     def __init__(self, model_weights: str):
-        self.model = joblib.load(model_weights)
+        # self.model = joblib.load(model_weights)
+        self.model = 0
 
     def predict(self,X,features_names):
-        return self.model.predict_proba(X)
+        # return self.model.predict_proba(X)
+        return "treat"
+
